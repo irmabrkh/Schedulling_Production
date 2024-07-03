3ea912c7389f388043bf0352a783f8802acd725f
Irma <irmab7449@gmail.com>
2024-07-03 13:09:05 +0700

    update

---
+++ app/src/main/java/com/example/scheduling_production/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.scheduling_production
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DISPLAY_LENGTH = 2000 // 2 detik
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val imageView: ImageView = findViewById(R.id.splash_logo)
+        val imageUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.logo_splash)
+        imageView.setImageURI(imageUri)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
